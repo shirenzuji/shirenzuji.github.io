44bbe1a65bbb0d6374e3ffb581b13871bbffe7ca
TanHan0813 <2087506519@qq.com>
2021-08-18 19:57:28 +0800

    1

---
+++ cu.js
@@ -0,0 +1,12 @@
+const a = document.querySelector("#a");
+const b = document.querySelector("#b");
+const cu = document.querySelector("#cu");
+
+let a_text, b_text, cu_text;
+
+function runf(){
+    a_text = a.textContent;
+    b_text = b.textContent;
+    cu_text = cu.textContent;
+    alert(a_text,b_text,cu_text);
+}
