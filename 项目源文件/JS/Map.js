ac8ed36089542a6fe8473d089b5d9efb8754eed9
TanHan0813 <2087506519@qq.com>
2021-10-02 13:21:30 +0800

    NEW

--- 项目源文件/JS/Map.js
+++ 项目源文件/JS/Map.js
@@ -1,8 +1,8 @@
-console.log("SUCCESS!");
+console.log("Map.js运行成功！");
 
 const show_years = document.querySelector(".SHOW_YEARS");
 const Input = document.getElementById("INPUT_TIME");
 show_years.textContent = "WAITING_FOR_INPUT";
 Input.addEventListener("input", () => {
-    show_years.textContent = Input.value;
+    show_years.textContent = "现在是：" + Input.value + "年";
 });
